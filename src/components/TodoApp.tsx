022f3d1f11eacd0744ce0a8ffa270ee7996708a8
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-05-28 10:50:49 +0200

    Add buttons

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -18,15 +18,29 @@ export const TodoApp = () => {
       <h1>TodoApp <span className="badge bg-danger rounded-pill">{todos.length}</span></h1>
       <hr />
 
-      <ul className="list-group list-group-flush">
-        {
-          todos.map((todo, idx) => (
-            <li key={todo.id} className="list-group-item">
-              {idx + 1}{todo.desc}
-            </li>
-          ))
-        }
-      </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {
+              todos.map((todo, idx) => (
+                <li key={todo.id} className="list-group-item">
+                  <p className="text-center" >{idx + 1} - {todo.desc}</p>
+                  <button className="btn btn-danger">Delete</button>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Add Todo</h4>
+          <hr />
+          <form action="post" className="d-grid gap-2">
+            <input className="form-control" type="text" name="description" placeholder="Learn..." autoCapitalize="off" />
+            <button className="btn btn-outline-primary mt-1">Add</button>
+          </form>
+        </div>
+
+      </div>
     </div>
   )
 }
