dded8a9d6dc11454f130d082a28e9eaf075ab639
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-05-28 17:42:45 +0200

    Modify TodoApp.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,7 +6,7 @@ export const TodoList = ({ todos, handleDelete, handleToggle }:any) => {
         <ul className="list-group list-group-flush" >
             {
                 todos.map((todo:ITodo, idx:number) => (
-                   <TodoListItem todo={todo} idx={idx} handleToggle={handleToggle} handleDelete={handleDelete} />
+                   <TodoListItem key={todo.id} todo={todo} idx={idx} handleToggle={handleToggle} handleDelete={handleDelete} />
                 ))
             }
         </ul>
